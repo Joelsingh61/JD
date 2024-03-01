1604896a51855b5d99bfec6266d509b21afe7256
Joelsingh61 <161309813+Joelsingh61@users.noreply.github.com>
2024-03-02 00:41:45 +0530

    jobdesc.py

--- jobdesc.py
+++ jobdesc.py
@@ -11,7 +11,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load Google Gemini Pro Vision API And get response
 
-def get_gemini_repsonse(input,image,prompt):
+def get_gemini_response(input,image,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([input,image[0],prompt])
     return response.text
@@ -77,6 +77,6 @@ matched with the selected job description , u have to mach the skill sets of res
 
 if submit:
     image_data=input_image_setup(uploaded_file)
-    response=get_gemini_repsonse(input_prompt,image_data,input)
+    response=get_gemini_response(input_prompt,image_data,input)
     st.subheader("The mattched job Description is ")
     st.write(response)
