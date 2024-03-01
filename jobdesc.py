ae4755a55ea06f01d311e77b3f56ed52f9063faa
Joelsingh61 <161309813+Joelsingh61@users.noreply.github.com>
2024-03-02 00:32:08 +0530

    jobdesc.py

--- jobdesc.py
+++ jobdesc.py
@@ -17,8 +17,6 @@ def get_gemini_repsonse(input,image,prompt):
     return response.text
 
 def input_image_setup(uploaded_file):
-   
-
     # Check if a file has been uploaded
     if uploaded_file is not None:
         # Read the file into bytes
@@ -40,7 +38,7 @@ st.set_page_config(page_title="JJD")
 
 st.header("JJD")
 input=st.text_input("Input Prompt: ",key="input")
- uploaded_file = st.file_uploader("Upload your resume", type=['jpg', 'pdf', 'png'])
+
 
 
 # List of all jobs (replace with your complete list)
@@ -53,18 +51,19 @@ all_jobs = [
 
 
 selected_job = st.selectbox('Select a job:', all_jobs)
+uploaded_file = st.file_uploader("Upload your resume", type=['jpg', 'pdf', 'png'])
 
 
 image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
-    uploaded_file = st.file_uploader("Upload your resume", type=['jpg', 'pdf', 'png'])
+
 
 submit=st.button("Check the Resume")
 
 input_prompt=f"""
-You are an expert in Job descriptery u should check the uploaded resume from user and  {selected_job} u have to analyze the resume and say how much it has been 
+You are an expert in Job descriptery u should check the uploaded resume from user and """  {selected_job}  """ u have to analyze the resume and say how much it has been 
 matched with the selected job description , u have to mach the skill sets of resume and other u need to predic the percenage and if he is not
  having that skill set u have to give him eha skills he need to develop
                ----
